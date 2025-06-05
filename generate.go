947f9927ebb64cd4b493300ab8763c947028b8df
Christopher Riley <rileyc@gmail.com>
2025-06-05 12:34:32 -0700

    use a template

--- generate.go
+++ generate.go
@@ -1,26 +1,55 @@
 package main
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"text/template"
+)
+
+const generatedFileTemplate string = `
+// generated file - do not modify
+
+package {{.PackageName}}
+
+type {{.StructName}} {{.StructDef}}
+
+var {{.VarName}} {{.StructName}} = {{.Instance}}
+`
+
+type Template struct {
+	PackageName string
+	StructName  string
+	StructDef   string
+	VarName     string
+	Instance    string
+}
 
 func Generate(rawJson, pkgName, structName, varName string) string {
 	var rawJsonMap map[string]any
 
 	if err := json.Unmarshal([]byte(rawJson), &rawJsonMap); err != nil {
-		panic(err)
+		panic(fmt.Sprintf("failed to unmarshal input json: %s", err))
 	}
 
 	goStruct := BuildGoStruct(rawJsonMap, structName, 0, "    ")
 	instance := GoInstance{*goStruct}
 
-	var ret string
+	tmpl := Template{
+		PackageName: pkgName,
+		StructName:  structName,
+		StructDef:   goStruct.String(),
+		VarName:     varName,
+		Instance:    instance.String(),
+	}
 
-	ret += "// generated file - do not modify" + "\n"
-	ret += "\n"
-	ret += "package " + pkgName + "\n"
-	ret += "\n"
-	ret += "type " + structName + " " + goStruct.String() + "\n"
-	ret += "\n"
-	ret += "var " + varName + " " + structName + " = " + instance.String() + "\n"
+	buf := bytes.Buffer{}
+	writer := io.Writer(&buf)
+	err := template.Must(template.New("gen").Parse(generatedFileTemplate)).Execute(writer, tmpl)
+	if err != nil {
+		panic(fmt.Sprintf("template generation failed: %s", err))
+	}
 
-	return ret
+	return buf.String()
 }
